f2d214c30a8894a5b32f21841447315748664d11
LongjiangLi <58618773+LongjiangLi@users.noreply.github.com>
2019-12-11 23:27:23 +0800

    Update Demo.java
    新增注释

--- Test/src/cn/com/hot/Demo.java
+++ Test/src/cn/com/hot/Demo.java
@@ -1,9 +1,10 @@
 package cn.com.hot;
 
 public class Demo {
-
+//hello
 	public static void main(String[] args) {
 		System.out.println("人间何处不青山");
 		System.out.println("good night!");
+          
 	}
 }
